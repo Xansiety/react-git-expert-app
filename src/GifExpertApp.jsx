9f54ab57a3280e9da7efe4a1e761b93906593f14
xansiety <ferando543@outlook.com>
2022-09-22 19:01:57 -0500

    Estilos globales

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,11 @@
 export const GifExpertApp = () => {
   return (
     <>
+    {/* Titulo */}
       <h1>Gif Expert APP</h1>
+      {/* input */}
+      {/* Listado de Gif */}
+        {/* GitItem */}
     </>
   );
 };
