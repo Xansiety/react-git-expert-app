e0bb46d0b2df8e0a3653445bc13346ea59866972
xansiety <ferando543@outlook.com>
2022-09-22 19:11:55 -0500

    Recorrer un array

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+
+
 export const GifExpertApp = () => {
+
+  const [categories, setCategories] = useState(['Naruto', 'Hunter x Hunter'])
+ 
+
   return (
     <>
     {/* Titulo */}
       <h1>Gif Expert APP</h1>
       {/* input */}
       {/* Listado de Gif */}
+      <ol>
+         { 
+         categories.map(category => {
+          return <li key={ category }>{category}</li>
+         }) 
+         }
+      </ol>
         {/* GitItem */}
     </>
   );
