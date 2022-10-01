d20818259b591a2309a037dbbaec17c088cd8817
xansiety <ferando543@outlook.com>
2022-09-30 19:13:16 -0500

    Validar que los elementos no se repitan

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -6,6 +6,9 @@ export const GifExpertApp = () => {
 
   const onAddCategory = (newCategory) => {
     // console.log({ newCategory });
+
+    if(categories.includes(newCategory)) return
+
     setCategories([newCategory, ...categories]);
     // setCategories( cat => [...cat, 'Term'] ) // 2da forma
   };
