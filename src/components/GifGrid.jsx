3974f159420c3d358e938b0867fd446e36a27cd8
xansiety <ferando543@outlook.com>
2022-09-30 20:54:31 -0500

    Loading in CustomHooks

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -5,13 +5,16 @@ export const GifGrid = ({ category }) => {
   // De esta forma se ejecuta cada vez que se redibuja el componente
   // getGifs(category);
 
-  const { images, isLoading } = useFetchGifs(category)
- 
+  const { images, isLoading } = useFetchGifs(category);
+
   // console.log({images, isLoading})
 
   return (
     <>
       <h3>{category}</h3>
+
+      {/* {isLoading ? <h2>Cargando...</h2> : null} */}
+      {isLoading && <h2>Cargando...</h2>}
 
       <div className="card-grid">
         {images.map((image) => (
