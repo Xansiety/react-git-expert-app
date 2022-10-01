7386a6fb6bc2c533b4f29381c5251465feade3f3
xansiety <ferando543@outlook.com>
2022-09-30 19:56:43 -0500

    useEffect para ejecutar solo una vez una funcion

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,9 +1,15 @@
+import { useEffect } from "react";
 import { getGifs } from "../helpers/getGifts";
 
-export const GifGrid = ({ category }) => {
-  
-  getGifs(category);
 
+export const GifGrid = ({ category }) => {
+  // De esta forma se ejecuta cada vez que se redibuja el componente
+  // getGifs(category);
+ 
+  useEffect( () => {
+    getGifs(category)
+  }, [])
+  
   return (
     <>
       <h3>{category}</h3>
